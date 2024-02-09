eddb183edfdfa638296bc9af390846df1ad6cb54
Marc Feldman <marcryanfeldman@me.com>
2024-02-09 13:28:21 -0800

    add src rh data and rh parser script

---
+++ scripts/parse_rh_transactions.py
@@ -0,0 +1,129 @@
+import argparse
+import csv
+import pandas as pd
+from collections import deque
+
+# ----------------------------------- User Inputs ----------------------------------
+numbers_file = "/Users/marc/Documents/Finances/Taxes/Stock_History/rh_transactions.numbers"
+csv_file = "/Users/marc/Documents/Finances/Taxes/Stock_History/rh_transactions.csv"
+# ----------------------------------------------------------------------------------
+
+#def convert_nmbrs_2_csv():
+  # To Do
+
+# parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", "--underlier", help="Name of underlier (stock ticker)")
+args = parser.parse_args()
+
+out_path = "~/Documents/Finances/Taxes/Stock_History/" + args.underlier + "_transactions.csv"
+
+# read input file (all transactions)
+#convert_nmbrs_2_csv()
+df = pd.read_csv (csv_file)
+
+# build df for desired underlier
+df_und=df[df['Instrument'] == args.underlier]
+df_und = df_und.reset_index()
+df_und = df_und.drop(columns=['index'])
+df_und['Amount'] = df_und['Amount'].replace(to_replace="\((\$.+\.[0-9][0-9])\)", value=r'-\1', regex=True)
+
+# fetch all buys/sells for desired underlier
+df_und_bs = df_und[(df_und['Trans Code'] == "Buy") | (df_und['Trans Code'] == "Sell") | (df_und['Trans Code'] == "SPL")]
+df_und_bs = df_und_bs.reset_index()
+df_und_bs = df_und_bs.drop(columns=['index'])
+df_und_bs['Amount'] = df_und_bs['Amount'].replace(to_replace=[',', '\$'], value=['', ''], regex=True)
+
+print(df_und_bs)
+
+# Build/iterate over deques for all purchase buckets
+date_deque = deque("")
+tmp_date_deque = deque("")
+quant_deque = deque()
+notional_deque = deque()
+
+for idx in reversed(df_und_bs.index):
+  if (df_und_bs.loc[idx]['Trans Code'] == "Buy"):
+    # buy = push to FIFO
+    date_deque.append(df_und_bs.loc[idx]['Activity Date'])
+    quant_deque.append(float(df_und_bs.loc[idx]['Quantity']))
+    notional_deque.append(float(df_und_bs.loc[idx]['Amount']))
+  elif (df_und_bs.loc[idx]['Trans Code'] == "Sell"):
+    # sell = pop from FIFO
+    print_str = "Sale on " + df_und_bs.loc[idx]['Activity Date'] + " (" + df_und_bs.loc[idx]['Quantity'] + " shares)"
+    print("---------------------", print_str, "---------------------")
+    remaining_quantity = float(df_und_bs.loc[idx]['Quantity'])
+    proceeds_per_share = float(df_und_bs.loc[idx]['Amount'])/remaining_quantity
+    pl = 0
+    while remaining_quantity > 0:
+      if (remaining_quantity >= quant_deque[0]):
+        # entire bucket will be popped
+
+        # grab quantity/amounts from the oldest purchase bucket
+        bought_quantity = quant_deque.popleft()
+        bought_notional = notional_deque.popleft()
+
+        # calculate cost basis per share of the bucket
+        cb_per_share = bought_notional/bought_quantity
+
+        # accumulate P/L and update remaining_quantity
+        pl += (proceeds_per_share + cb_per_share) * bought_quantity
+        remaining_quantity -= bought_quantity
+
+        # accumulate bucket dates that are ammortized
+        #tmp_date_deque.append(date_deque.popleft())
+
+        print(date_deque.popleft(), "bucket is gone. ", remaining_quantity, "shares remain. PL =", (proceeds_per_share + cb_per_share) * bought_quantity)
+      else:
+        # Tail of FIFO will be modified
+
+        # calculate cost basis per share of the bucket in use
+        cb_per_share = notional_deque[0]/quant_deque[0]
+
+        # not entire quantity will be ammortized- just remaining quantity
+        bought_quantity = remaining_quantity
+
+        # update FIFO tails
+        quant_deque[0] -= bought_quantity
+        notional_deque[0] -= cb_per_share * bought_quantity
+
+        # accumulate P/L and update remaining_quantity
+        pl += (proceeds_per_share + cb_per_share) * bought_quantity
+        remaining_quantity -= bought_quantity
+
+        print(date_deque[0], "bucket has", quant_deque[0], "shares remaining. PL =", (proceeds_per_share + cb_per_share) * bought_quantity)
+
+    print("P/L:", pl)
+
+  elif (df_und_bs.loc[idx]['Trans Code'] == "SPL"):
+    cum_shares = 0
+    add_shares = float(df_und_bs.loc[idx]['Quantity'])
+    
+    # calculate total shares owned
+    for d_i in range(len(quant_deque)):
+      cum_shares += quant_deque[d_i]
+
+    # calculate split factor
+    split = (cum_shares + add_shares)/cum_shares
+    print("split factor:", split)
+
+    # update bucket quantities
+    for d_i in range(len(quant_deque)):
+      quant_deque[d_i] *= split
+
+  elif (df_und_bs.loc[idx]['Trans Code'] == "ACATS"):
+    # build df from deques
+
+
+    # rearrange df by date
+
+
+    # build updated deques
+
+
+# export desired underlier info to file
+#df_und.to_csv(out_path, index=False)
+
+
+
+
